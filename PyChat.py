8756cd2b70d01a7447885666dd5ea598979611ad
Bill Jellesma <william.jellesma@yahoo.com>
2015-08-20 22:04:49 -0400

    Add GUI

--- PyChat.py
+++ PyChat.py
@@ -1,26 +1,73 @@
 import random
+from Tkinter import *
 
-def conversation(member):
-    random.seed()
-    randNum = random.randint(0, 2)
-    if member == 'Bill':
-        print billArray[randNum]
-    if member == 'Jeff':
-        print jeffArray[randNum]
-    if member == 'Matt':
-        print mattArray[randNum]
-    confirm = raw_input('Would you like to talk to anyone else?')
-    if confirm == 'yes':
-        member = raw_input('Who?')
-        conversation(member)
-    if confirm == 'no':
-        return
-    return
+class Application(Frame):
+    
+    billArray = ["Hi", "What\'d you say to me?", "Do I look like I drive cabs in Baton Rouge for fun?"]
+    jeffArray = ["Yo", "Dude, that's so funny I forgot to laugh", "You know, you would get along well with my grandmother, she is really annoying too"]
+    mattArray = ["Lemonade for sale", "You think you are better than me", "My dad could so beat your dad in a race"]                    
 
-billArray = ["Hi", "What\'d you say to me?", "Do I look like I drive cabs in Baton Rouge for fun?"]
-jeffArray = ["Yo", "Dude, that's so funny I forgot to laugh", "You know, you would get along well with my grandmother, she is really annoying too"]
-mattArray = ["Lemonade for sale", "You think you are better than me", "My dad could so beat your dad in a race"]
+    def __init__(self, master):
+        #initialize frame
+        Frame.__init__(self,master)
+        self.grid()
+        
+        #init arrays
+        
+        self.create_widgets()
 
-member = raw_input('Hi, who would you like to talk to?')
-conversation(member)
+    def generateRand(self):
+        
+        return randNum
+    
+    def billArrayFunction(self):
+        #Start randNum
+        random.seed()
+        randNum = random.randint(0, 2)
+        self.billLabel = Label(self)
+        self.billLabel.grid()
+        self.billLabel["text"] = self.billArray[randNum]
 
+    def jeffArrayFunction(self):
+        #Start randNum
+        random.seed()
+        randNum = random.randint(0, 2)
+        self.jeffLabel = Label(self)
+        self.jeffLabel.grid()
+        self.jeffLabel["text"] = self.jeffArray[randNum]
+
+    def mattArrayFunction(self):
+        #Start randNum
+        random.seed()
+        randNum = random.randint(0, 2)
+        self.mattLabel = Label(self)
+        self.mattLabel.grid()
+        self.mattLabel["text"] = self.mattArray[randNum]
+
+    def create_widgets(self):
+        #create buttons
+        self.billButton = Button(self)
+        self.billButton.grid()
+        self.billButton["text"] = "Talk to Bill"
+        self.billButton["command"] = self.billArrayFunction
+
+        self.jeffButton = Button(self)
+        self.jeffButton.grid()
+        self.jeffButton["text"] = "Talk to Jeff"
+        self.jeffButton["command"] = self.jeffArrayFunction
+
+        self.mattButton = Button(self)
+        self.mattButton.grid()
+        self.mattButton["text"] = "Talk to Matt"
+        self.mattButton["command"] = self.mattArrayFunction
+
+
+
+
+root = Tk()
+root.title("buttons")
+root.geometry("1000x800")
+
+app = Application(root)
+
+root.mainloop()
